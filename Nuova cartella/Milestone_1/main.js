13c74aca50f550fa1c19691ebfb13d714146e3d7
NicolaL95 <nicola.lilla@gmail.com>
2021-10-27 20:03:40 +0200

    Aggiunta colorazione dinamica icone

--- Nuova cartella/Milestone_1/main.js
+++ Nuova cartella/Milestone_1/main.js
@@ -109,26 +109,11 @@ icons.forEach((element,index) => {
      selRow = document.querySelector(`.row_${index / 5}`)
 }
 /* aggiunta cards all'elemento row */
-if(icons[index].type == "animal"){ 
-    cards = `<div class="cards">
-    <i class="${icons[index].family} ${icons[index].prefix}${icons[index].name} blue_icon"></i>
-    <p class="icon_text">${icons[index].name}</p>
-    </div>`
-}
-
-else if(icons[index].type == "vegetable"){ 
-    cards = `<div class="cards">
-    <i class="${icons[index].family} ${icons[index].prefix}${icons[index].name} gold_icon"></i>
-    <p class="icon_text">${icons[index].name}</p>
-    </div>`
-}
-else{
-    cards = `<div class="cards">
-<i class="${icons[index].family} ${icons[index].prefix}${icons[index].name} purple_icon"></i>
+cards = `<div class="cards">
+<i class="${icons[index].family} ${icons[index].prefix}${icons[index].name}"></i>
 <p class="icon_text">${icons[index].name}</p>
 </div>`
-}
-
+console.log(cards)
 selRow.innerHTML += cards;
 });
 
